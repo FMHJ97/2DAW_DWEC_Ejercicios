aec6d163d074261b1a61977ba3208d4479174bcb
FMHJ97 <francisco-h-j@hotmail.com>
2024-10-15 10:29:17 +0200

    feat(JavaScript ES6): apuntes updated.

---
+++ JavaScript/Apuntes/4. Javascript ES6/5. Funcionalidad de Modulos/main.js
@@ -0,0 +1,6 @@
+import sumar from "./modulo.js";
+sumar(5,10);
+
+// En el caso de nombrar la función de modulo.js
+// import {sumar} from "./modulo.js";
+// sumar(5,10);
---
+++ JavaScript/Apuntes/4. Javascript ES6/5. Funcionalidad de Modulos/modulo.js
@@ -0,0 +1,10 @@
+export default function(a,b) {
+    console.log(a + " + " + b + " = " + (a+b));
+}
+
+// Si el ponemos un nombre a la función (sumar),
+// debemos llamarla de esa manera obligatoriamente.
+
+// export default function sumar(a,b) {
+//     console.log(a + " + " + b + " = " + (a+b));
+// }
