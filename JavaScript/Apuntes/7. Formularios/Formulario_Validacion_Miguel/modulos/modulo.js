0948abef10ac4d5309ac0767c07e8cd7d00c24f0
FMHJ97 <francisco-h-j@hotmail.com>
2024-11-17 11:32:06 +0100

    feat(Relacion_07): Ejercicio 1 added

--- JavaScript/Apuntes/7. Formularios/Formulario_Validacion_Miguel/modulos/modulo.js
+++ JavaScript/Apuntes/7. Formularios/Formulario_Validacion_Miguel/modulos/modulo.js
@@ -1,4 +1,4 @@
-import {validacion} from "../js/validaciones_formulario2.js";
+import {validacion} from "../js/validaciones_formulario2";
 
 document.addEventListener("DOMContentLoaded",e=>{
     validacion();
