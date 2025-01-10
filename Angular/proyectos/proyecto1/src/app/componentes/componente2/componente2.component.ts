c4181f79187bc29e76c9bca05a8ff93fb6aa4e14
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-01-10 14:01:23 +0100

    feat(Angular): files added

---
+++ Angular/proyectos/proyecto1/src/app/componentes/componente2/componente2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente2',
+  imports: [],
+  template: '<h2>Soy el componente 2</h2>',
+  styleUrl: './componente2.component.css'
+})
+export class Componente2Component {
+
+}
